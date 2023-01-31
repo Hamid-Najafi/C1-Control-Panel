af2e3a069b2dd52aadb70a68550f30d39f792f82
Hamid <Hamid.Najafi@email.com>
2023-01-31 18:53:11 +0330

    init

--- ControlPanel-Install.sh
+++ ControlPanel-Install.sh
@@ -35,28 +35,29 @@ timedatectl set-timezone Asia/Tehran
 echo "-------------------------------------"
 echo "Installing Pre-Requirements"
 echo "-------------------------------------"
-string="http://ir.archive.ubuntu.com/ubuntu"
-file="/etc/apt/sources.list"
-if grep -q "$string" "$file"; then
-  echo "Replacing APT Sources File"
-  mv /etc/apt/sources.list{,.backup}
-  sh -c "echo 'deb [trusted=yes] https://debian.iranrepo.ir bullseye main' >> /etc/apt/sources.list"
-  # sh -c "echo 'deb [trusted=yes] https://debian.iranrepo.ir jammy main' >> /etc/apt/sources.list"
-  sudo apt-key adv --keyserver debian.iranrepo.ir --recv-keys  648ACFD622F3D138
-  sudo apt-key adv --keyserver debian.iranrepo.ir --recv-keys  0E98404D386FA1D9
-  sudo apt-key adv --keyserver debian.iranrepo.ir --recv-keys  605C66F00D6C9793
-  # wget https://raw.githubusercontent.com/Hamid-Najafi/DevOps-Notebook/master/Apps/Apt/amd64-sources.list -O /etc/apt/sources.list
-  # wget https://raw.githubusercontent.com/Hamid-Najafi/DevOps-Notebook/master/Apps/Apt/arm64-sources.list -O /etc/apt/sources.list
-fi
-string="http://archive.ubuntu.com/ubuntu"
-if grep -q "$string" "$file"; then
-  echo "Replacing APT Sources File"
-  mv /etc/apt/sources.list{,.backup}
-  sh -c "echo 'deb [trusted=yes] https://debian.iranrepo.ir bullseye main' >> /etc/apt/sources.list"
-  # sh -c "echo 'deb [trusted=yes] https://debian.iranrepo.ir jammy main' >> /etc/apt/sources.list"
-  # wget https://raw.githubusercontent.com/Hamid-Najafi/DevOps-Notebook/master/Apps/Apt/amd64-sources.list -O /etc/apt/sources.list
-  # wget https://raw.githubusercontent.com/Hamid-Najafi/DevOps-Notebook/master/Apps/Apt/arm64-sources.list -O /etc/apt/sources.list
-fi
+# string="http://ir.archive.ubuntu.com/ubuntu"
+# file="/etc/apt/sources.list"
+# if grep -q "$string" "$file"; then
+#   echo "Replacing APT Sources File"
+#   mv /etc/apt/sources.list{,.backup}
+#   sh -c "echo 'deb [trusted=yes] https://debian.iranrepo.ir bullseye main' >> /etc/apt/sources.list"
+#   # sh -c "echo 'deb [trusted=yes] https://debian.iranrepo.ir jammy main' >> /etc/apt/sources.list"
+#   sudo apt-key adv --keyserver debian.iranrepo.ir --recv-keys  648ACFD622F3D138
+#   sudo apt-key adv --keyserver debian.iranrepo.ir --recv-keys  0E98404D386FA1D9
+#   sudo apt-key adv --keyserver debian.iranrepo.ir --recv-keys  605C66F00D6C9793
+#   # wget https://raw.githubusercontent.com/Hamid-Najafi/DevOps-Notebook/master/Apps/Apt/amd64-sources.list -O /etc/apt/sources.list
+#   # wget https://raw.githubusercontent.com/Hamid-Najafi/DevOps-Notebook/master/Apps/Apt/arm64-sources.list -O /etc/apt/sources.list
+# fi
+# string="http://archive.ubuntu.com/ubuntu"
+# if grep -q "$string" "$file"; then
+#   echo "Replacing APT Sources File"
+#   mv /etc/apt/sources.list{,.backup}
+#   sh -c "echo 'deb [trusted=yes] https://debian.iranrepo.ir bullseye main' >> /etc/apt/sources.list"
+#   # sh -c "echo 'deb [trusted=yes] https://debian.iranrepo.ir jammy main' >> /etc/apt/sources.list"
+#   # wget https://raw.githubusercontent.com/Hamid-Najafi/DevOps-Notebook/master/Apps/Apt/amd64-sources.list -O /etc/apt/sources.list
+#   # wget https://raw.githubusercontent.com/Hamid-Najafi/DevOps-Notebook/master/Apps/Apt/arm64-sources.list -O /etc/apt/sources.list
+# fi
+
 export DEBIAN_FRONTEND=noninteractive
 apt update && apt upgrade -q -y
 apt install -q -y software-properties-common git avahi-daemon python3-pip nano lame sox libsox-fmt-mp3 curl zip unzip atop bmon
@@ -189,12 +190,12 @@ folder="/home/c1tech/C1-Control-Panel"
 git clone "${url}" "${folder}"
 folder="/home/c1tech/C1"
 [ -d "${folder}" ] && rm -rf "${folder}"    
-mv /home/c1tech/C1-Control-Panel/C1 /home/c1tech/
 cd /home/c1tech/C1-Control-Panel/Panel
 touch -r *.*
 qmake
 make -j4 
 
+mv /home/c1tech/C1-Control-Panel/C1 /home/c1tech/
 chown -R c1tech:c1tech /home/c1tech/C1
 chown -R c1tech:c1tech /home/c1tech/C1-Control-Panel
 chmod +x /home/c1tech/C1/ExecStart.sh
