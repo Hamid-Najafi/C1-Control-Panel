c3a7103732b32c4a405a691d65b6e67aab72e7aa
Hamid <Hamid.Najafi@email.com>
2023-02-05 19:57:22 +0330

    init

--- ControlPanel-Install.sh
+++ ControlPanel-Install.sh
@@ -29,9 +29,11 @@ if ! grep -q "$string" "$file"; then
   printf "\n%s" "127.0.0.1 $hostname" >> "$file"
 fi
 echo "-------------------------------------"
-echo "Setting TimeZone"
+echo "Setting TimeZone & Locale"
 echo "-------------------------------------"
 timedatectl set-timezone Asia/Tehran 
+locale-gen fa_IR
+locale-gen
 echo "-------------------------------------"
 echo "Installing Pre-Requirements"
 echo "-------------------------------------"
@@ -244,12 +246,18 @@ update-grub
 
 apt -y autoremove --purge plymouth
 apt -y install plymouth plymouth-themes
-# By default ubuntu-text is active ÷
+# By default ubuntu-text is active 
 # /usr/share/plymouth/themes/ubuntu-text/ubuntu-text.plymouth
 # We Will use bgrt (which is same as spinner but manufacture logo is enabled) theme with our custom logo
-cp /home/c1tech/C1-Control-Panel/bgrt-c1.png /usr/share/plymouth/themes/spinner/bgrt-fallback.png
-cp /home/c1tech/C1-Control-Panel/watermark-empty.png /usr/share/plymouth/themes/spinner/watermark.png
-cp /home/c1tech/C1-Control-Panel/watermark-empty.png /usr/share/plymouth/ubuntu-logo.png
+cp /usr/share/plymouth/themes/spinner/bgrt-fallback.png{,.bak}
+cp /usr/share/plymouth/themes/spinner/watermark.png{,.bak}
+cp /usr/share/plymouth/ubuntu-logo.png{,.bak}
+
+# This Comes abow Spinner
+cp /home/c1tech/C1-Control-Panel/bgrt-c1.png /usr/share/plymouth/ubuntu-logo.png
+# This Comes bellow Spinner
+cp /home/c1tech/C1-Control-Panel/bgrt-c1.png /usr/share/plymouth/themes/spinner/watermark.png
+
 update-initramfs -u
 # update-alternatives --list default.plymouth
 # update-alternatives --display default.plymouth
